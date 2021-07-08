ade24fe33c02066559e172e0cb88dcfa80a15bce
Eduard Kruger <eduard.kruger@gmail.com>
2021-07-08 17:03:33 +0200

    Section 5 continued.
    Section 5 continued.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './_services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +12,23 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService){
 
   }
 
   ngOnInit() {
     this.getUsers();
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      const user: User = JSON.parse(userData);
+      this.accountService.setCurrentUser(user);
+    }
+  }
+
 
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -16,6 +16,7 @@ export class NavComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
@@ -29,6 +30,15 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => {
+      console.log(error);
+    });
   }
 }
