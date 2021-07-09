6356f9d84c67442fee9f5787ac6363682c5a3a1c
Eduard Kruger <eduard.kruger@gmail.com>
2021-07-09 10:11:14 +0200

    User management slight change.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -7,23 +9,20 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   model: any = {};
-  loggedIn: boolean = false;
-
-  constructor(private accountService: AccountService) {
+  currentUser$: Observable<User> | undefined;
+  constructor(public accountService: AccountService) {
 
   }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+
   }
 
   login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
@@ -31,14 +30,5 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    });
   }
 }
